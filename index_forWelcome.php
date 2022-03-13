cc977bab8883a8215f3ad2ad76162093ee7873f7
Anuramana <101526325+Anuramana@users.noreply.github.com>
2022-03-13 16:38:11 -0500

    Add files via upload

---
+++ index_forWelcome.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+Student ID: <input type="number" name="id"><br>
+name: <input type="text" name="name"><br>
+Department Name: <input type = "text" name = "dept_name"><br>
+Total credits: <input type = "number" name = "tot_cred"><br>
+
+<input type="submit">
+</form>
+
+</body>
+</html>
